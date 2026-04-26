0bad0fb7db42203e461cae0094adbf01c0bbe56d
11249a290-glitch <11249a290@kanchiuniv.ac.in>
2026-04-26 22:32:13 +0530

    Update Implement Fractional Knapsack using greedy method .c

--- DAA LAB/Implement Fractional Knapsack using greedy method .c
+++ DAA LAB/Implement Fractional Knapsack using greedy method .c
@@ -3,8 +3,9 @@ struct item
 {
     int profit;
     int weight;
-    float ratio;
+    float ratio;//profit per weight ratio
 };
+ /* Function to sort items by ratio in descending order */ 
 void sort(struct item items[],int n)
 {
     struct item temp;
@@ -24,17 +25,21 @@ void sort(struct item items[],int n)
 int main()
 {
     int c,n;
+    //caacity 
     printf("enter capacity:");
     scanf("%d",&c);
+    //no.of items
      printf("enter no of items:");
      scanf("%d",&n);
      struct item itm[n];
      for(int i=0;i<n;i++)
      {
+         //weights 
         printf("enter item %d weight profit:",i+1);
         scanf("%d" "%d",&itm[i].weight,&itm[i].profit);
         itm[i].ratio=(itm[i].profit)/(itm[i].weight);
      }
+    /* Sort items by value/weight ratio */ 
      sort(itm,n);
      float totalProfit=0.0;
      for(int i=0;i<n;i++)
