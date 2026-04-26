f2d9beef3da7a5768763074714f41de923fcb3c4
11249a290-glitch <11249a290@kanchiuniv.ac.in>
2026-04-26 22:48:45 +0530

    Update Implement nth Fibonacci term using recursion & iteration.c

--- DAA LAB/Implement nth Fibonacci term using recursion & iteration.c
+++ DAA LAB/Implement nth Fibonacci term using recursion & iteration.c
@@ -2,21 +2,30 @@
 int fib(int n)
 {
 //default conditions
+/* Base Condition: If n is 0 or 1, the term is the value itself[cite: 141]. 
+ */
 if(n==0)
 return 0;
 if(n==1)
 return 1;
 //to print the nth fibonacci term
+  /* * Recursive Step: F(n) = F(n-1) + F(n-2)[cite: 142]. 
+    * Note: This causes the function to branch twice for every call, 
+    * leading to the exponential time complexity observed in DAA. 
+    */ 
 return fib(n-1)+fib(n-2);
 }
 int main()
 {
 int n;
+  // Step 1: Read the input n
 printf("enter number: ");
 scanf("%d",&n);
+ /* Checking if input is a valid integer and non-negative. */ 
 if (n < 0) {
 printf("invalid");
 }
+ //function call
 fib(n);
 printf("fibonacci term is: %d", fib(n));
 return 0;
