2e2bbb3b5747d4f572b690be8dcac501c99b1ca9
11249a290-glitch <11249a290@kanchiuniv.ac.in>
2026-08-20 12:43:39 +0530

    Add files via upload

---
+++ JAVA_LAB/PACKAGE/Main.java
@@ -0,0 +1,7 @@
+import studentInfo.Student;
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Alex", 16);
+        student.displayInfo();
+    }
+}
